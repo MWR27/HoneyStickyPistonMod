8f4dc8f8dd4a5d26d8369c84331259d89cf5ed18
MWR27 <64335495+MWR27@users.noreply.github.com>
2022-05-20 19:17:32 -0400

    Update to 1.18

--- src/main/java/mwr_/honeystickypistonmod/tileentity/ModBlockEntityType.java
+++ src/main/java/mwr_/honeystickypistonmod/tileentity/ModBlockEntityType.java
@@ -3,9 +3,9 @@ package mwr_.honeystickypistonmod.tileentity;
 import mwr_.honeystickypistonmod.HoneyStickyPistonMod;
 import mwr_.honeystickypistonmod.block.ModBlocks;
 import net.minecraft.world.level.block.entity.BlockEntityType;
-import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModBlockEntityType {
 	public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES = DeferredRegister
